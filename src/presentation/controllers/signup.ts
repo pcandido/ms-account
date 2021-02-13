28621f1a5228caab3478b8a89d364f58c32f90ec
Paulo Cândido <paulo.candido@g.globo>
2021-02-13 20:54:55 -0300

    feat: add email validation to SignUpController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,13 +2,22 @@ import { Controller } from '@presentation/protocols/controllers'
 import { HttpRequest, HttpResponse } from '@presentation/protocols/http'
 import { MissingParamError } from '@presentation/errors/missing-param-error'
 import { badRequest } from '@presentation/helpers/http-helper'
+import { EmailValidator } from '@presentation/protocols/email-validator'
+import { InvalidParamError } from '@presentation/errors/invalid-param-error'
 
 export class SignUpController implements Controller {
+
+  constructor(private emailValidator: EmailValidator) { }
+
   handle(request: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!request.body[field])
         return badRequest(new MissingParamError(field))
+    }
+
+    if (!this.emailValidator.isValid(request.body.email)) {
+      return badRequest(new InvalidParamError('email'))
     }
 
     return { statusCode: 200, body: {} }
---
+++ src/presentation/errors/invalid-param-error.ts
@@ -0,0 +1,8 @@
+export class InvalidParamError extends Error {
+
+  constructor(paramName: string) {
+    super(`Invalid param: ${paramName}`)
+    this.name = this.constructor.name
+  }
+
+}
