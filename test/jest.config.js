b6aa1824e6688d44b59b6bf982613070a68d1e23
Paulo Cândido <paulo.candido@g.globo>
2021-02-13 18:13:46 -0300

    test: add structure for e2e tests

---
+++ test/jest.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { resolve } = require('path')
+
+const root = resolve(__dirname, '..')
+const rootConfig = require(`${root}/jest.config.js`)
+
+module.exports = {
+  ...rootConfig,
+  rootDir: root,
+  displayName: 'e2e',
+  setupFilesAfterEnv: ['<rootDir>/test/jest-setup.ts'],
+  testMatch: ['<rootDir>/test/**/*.test.ts'],
+}
