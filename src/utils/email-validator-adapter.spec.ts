aa4954c161671f294fbd8414900af9f99f14520f
Paulo Cândido <paulo.candido@g.globo>
2021-02-16 10:57:26 -0300

    test: refactor EmailValidatorAdapter test

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -3,29 +3,27 @@ import validator from 'validator'
 
 describe('EmailValidatorAdapter', () => {
 
-  jest.mock('validator', () => ({
-    isEmail(): boolean {
-      return true
-    },
-  }))
+  const makeValidatorSpy = (mockedReturn = true) => jest.spyOn(validator, 'isEmail').mockReturnValueOnce(mockedReturn)
+  const makeSut = () => new EmailValidatorAdapter()
 
   it('should return false if Validator returns false', () => {
-    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
-    const sut = new EmailValidatorAdapter()
+    makeValidatorSpy(false)
+    const sut = makeSut()
     const isValidEmail = sut.isValid('invailid_email@mail.com')
     expect(isValidEmail).toBeFalsy()
   })
 
   it('should return true if Validator returns true', () => {
-    const sut = new EmailValidatorAdapter()
+    makeValidatorSpy(true)
+    const sut = makeSut()
     const isValidEmail = sut.isValid('valid_email@mail.com')
     expect(isValidEmail).toBeTruthy()
   })
 
   it('should call EmailValidator with correct email', () => {
     const givenEmail = 'any_email@mail.com'
-    const isEmailSpy = jest.spyOn(validator, 'isEmail')
-    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = makeValidatorSpy()
+    const sut = makeSut()
     sut.isValid(givenEmail)
     expect(isEmailSpy).toHaveBeenCalledWith(givenEmail)
   })
