ded5f1c68bf6428164cbc7f48c035e0f029dc717
Paulo Cândido <paulo.candido@g.globo>
2021-02-18 22:18:26 -0300

    test: refactor DbAddAccount makeSut

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,6 +1,25 @@
+import { Encrypter } from '@data/protocols/encrypter'
 import { DbAddAccount } from './db-add-account'
 
 describe('DbAddAccount', () => {
+
+  interface SutTypes {
+    sut: DbAddAccount,
+    encrypterStub: Encrypter
+  }
+
+  const makeSut = (): SutTypes => {
+    class EncrypterStub {
+      async encrypt(value: string): Promise<string> {
+        return Promise.resolve('hashed_password')
+      }
+    }
+
+    const encrypterStub = new EncrypterStub()
+    const sut = new DbAddAccount(encrypterStub)
+
+    return { sut, encrypterStub }
+  }
 
   it('should call Encrypter with correct password', async () => {
 
@@ -11,18 +30,10 @@ describe('DbAddAccount', () => {
       password: givenPassword,
     }
 
-    class EncrypterStub {
-      async encrypt(value: string): Promise<string> {
-        return Promise.resolve('hashed_password')
-      }
-    }
-
-    const encrypterStub = new EncrypterStub()
+    const { sut, encrypterStub } = makeSut()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
 
-    const sut = new DbAddAccount(encrypterStub)
     await sut.add(givenAccount)
-
     expect(encryptSpy).toHaveBeenCalledWith(givenPassword)
 
   })
