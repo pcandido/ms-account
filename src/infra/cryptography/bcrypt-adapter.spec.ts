12e5b309f4d1ee8dc4b2e9a794996b1b6c5494f7
Paulo Cândido <paulo.candido@g.globo>
2021-02-18 23:37:45 -0300

    test: ensure BCryptAdapter returns the generated hash on success

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,7 +1,16 @@
 import { BCryptAdapter } from './bcrypt-adapter'
 import bcrypt from 'bcrypt'
 
+const givenGeneratedHash = 'generated_hash'
+
+jest.mock('bcrypt', () => ({
+  hash() {
+    return Promise.resolve(givenGeneratedHash)
+  },
+}))
+
 describe('BCryptAdapter', () => {
+
 
   it('should call BCrypt with correct values', async () => {
     const givenValue = 'some_value'
@@ -10,6 +19,14 @@ describe('BCryptAdapter', () => {
     const sut = new BCryptAdapter(givenSalt)
     await sut.encrypt(givenValue)
     expect(hashSpy).toBeCalledWith(givenValue, givenSalt)
+  })
+
+  it('should return the generated hash on success', async () => {
+    const givenValue = 'some_value'
+    const givenSalt = 12
+    const sut = new BCryptAdapter(givenSalt)
+    const hash = await sut.encrypt(givenValue)
+    expect(hash).toBe(givenGeneratedHash)
   })
 
 })
