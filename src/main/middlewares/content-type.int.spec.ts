05bad227af7fab5df82f7c03bcd419b52b8557c4
Paulo Cândido <paulo.candido@g.globo>
2021-02-20 11:39:21 -0300

    test: ensure request return forced content type

--- src/main/middlewares/content-type.int.spec.ts
+++ src/main/middlewares/content-type.int.spec.ts
@@ -15,5 +15,18 @@ describe('ContentType Middleware', () => {
       .expect('Content-Type', /json/)
   })
 
+  it('should return ContentType as text when forced', async () => {
+    const givenRoute = '/content_type_text_route'
+
+    app.get(givenRoute, (req, res) => {
+      res.type('text')
+      res.send('')
+    })
+
+    await request(app)
+      .get(givenRoute)
+      .expect('Content-Type', /text/)
+  })
+
 })
 
