381194ffd5c669aee824ca9a269f0954307be1de
Paulo Cândido <paulo.candido@g.globo>
2021-02-20 11:17:57 -0300

    feat: ensure request body is parsed as json

---
+++ src/main/config/middlewares.ts
@@ -0,0 +1,6 @@
+import { Express } from 'express'
+import { bodyParser } from '@main/middlewares'
+
+export default (app: Express): void => {
+  app.use(bodyParser)
+}
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,4 @@
-import express from 'express'
-
-const app = express()
+import app from './config/app'
 
 app.listen(3000, () => {
   console.log('Server runing at http://localhost:3000')
