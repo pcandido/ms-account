f094299691c1fca66d4cf403136ea4e9ea3915c5
Paulo Cândido <paulo.candido@g.globo>
2021-02-20 11:18:07 -0300

    test: ensure request body is parsed as json

---
+++ src/main/middlewares/body-parser.int.spec.ts
@@ -0,0 +1,24 @@
+import request from 'supertest'
+import app from '@main/config/app'
+
+describe('BodyParserMiddleware', () => {
+
+  it('should parser body as json', async () => {
+    const givenRoute = '/body_parser_route'
+    const givenBody = {
+      field1: 1,
+      field2: '2',
+    }
+
+    app.post(givenRoute, (req, res) => {
+      res.send(req.body)
+    })
+
+    await request(app)
+      .post(givenRoute)
+      .send(givenBody)
+      .expect(givenBody)
+  })
+
+})
+
