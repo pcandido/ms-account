6b9398441217f0f7a2abb3e948afd5c4d98eacd0
Paulo Cândido <paulo.candido@g.globo>
2021-02-21 20:02:19 -0300

    refactor: improve the server module

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,24 @@
 import '../utils/module-aliases'
-import app from '@main/config/app'
 import config from 'config'
+import { MongoHelper } from '@infra/db/mongodb/helpers/mogodb-helper'
 
-const port = config.get('app.port')
 
-app.listen(port, () => {
-  console.log(`Server runing at http://localhost:${port}`)
-})
+async function start() {
+
+  const mongodbUrl = config.get<string>('mongodb.url')
+  await MongoHelper.connect(mongodbUrl)
+
+  const port = config.get('app.port')
+  const app = (await import('@main/config/app')).default
+
+  app.listen(port, () => {
+    console.log(`Server runing at http://localhost:${port}`)
+  })
+
+}
+
+try {
+  start()
+} catch (e) {
+  console.error(e)
+}
