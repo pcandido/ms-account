b136ce40bfaf0ea063d88860ab7bb4993a52a137
Paulo Cândido <paulo.candido@g.globo>
2021-02-27 15:38:04 -0300

    test: create a builder to login request on tests

--- src/presentation/controllers/login/login.unit.test.ts
+++ src/presentation/controllers/login/login.unit.test.ts
@@ -11,15 +11,39 @@ const makeSut = (): SutTypes => {
   return { sut }
 }
 
+interface LoginBody {
+  email: string
+  password: string
+}
+
+const makeRequest = () => ({
+  request: {
+    body: {
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    } as LoginBody,
+  },
+
+  with(key: keyof LoginBody, value: string) {
+    this.request.body[key] = value
+    return this
+  },
+
+  without(key: keyof LoginBody) {
+    delete this.request.body[key]
+    return this
+  },
+
+  build(): { body: Partial<LoginBody> } {
+    return this.request
+  },
+})
+
 describe('Login Controller', () => {
 
   it('should return 400 if no email is provided', async () => {
     const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        password: 'any_password',
-      },
-    }
+    const httpRequest = makeRequest().without('email').build()
 
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
@@ -27,11 +51,7 @@ describe('Login Controller', () => {
 
   it('should return 400 if no password is provided', async () => {
     const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        email: 'any_email@mail.com',
-      },
-    }
+    const httpRequest = makeRequest().without('password').build()
 
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
