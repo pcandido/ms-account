6deb1af97464424ae37607c0bf478e400210ff01
Paulo Cândido <paulo.candido@g.globo>
2021-02-27 10:44:37 -0300

    feat: ensure ControllerLogger log internal errors

--- src/presentation/decorators/controller-logger.ts
+++ src/presentation/decorators/controller-logger.ts
@@ -1,11 +1,19 @@
 import { Controller, HttpRequest, HttpResponse } from '@presentation/protocols'
+import { Logger } from '@utils/logger'
 
 export class ControllerLogger implements Controller {
 
-  constructor(private controller: Controller) { }
+  constructor(
+    private controller: Controller,
+    private logger: Logger,
+  ) { }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const response = this.controller.handle(httpRequest)
+    const response = await this.controller.handle(httpRequest)
+    if (response.statusCode === 500) {
+      this.logger.error(response.body.cause || response.body)
+    }
+
     return response
   }
 
