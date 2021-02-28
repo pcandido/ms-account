00d4aafedd0054113924c88db6f2de6c15508a25
Paulo Cândido <paulo.candido@g.globo>
2021-02-27 23:27:34 -0300

    fix: update the main layer with new validation structure

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -6,13 +6,15 @@ import { ConsoleLoggerAdapter } from '@utils/console-logger-adapter'
 import { SignUpController } from '@presentation/controllers/signup/signup'
 import { Controller } from '@presentation/protocols'
 import { ControllerLogger } from '@presentation/decorators/controller-logger'
+import { signupValidator } from '@presentation/controllers/signup/signup-validator'
 
 export const makeSignUpController = (): Controller => {
   const emailValidator = new EmailValidatorAdapter()
   const encrypter = new BCryptAdapter()
   const addAccountRepository = new AccountMongoRepository()
   const addAccount = new DbAddAccount(encrypter, addAccountRepository)
-  const signupController = new SignUpController(emailValidator, addAccount)
+  const validator = signupValidator(emailValidator)
+  const signupController = new SignUpController(addAccount, validator)
   const consoleLoggerAdapter = new ConsoleLoggerAdapter()
   return new ControllerLogger(signupController, consoleLoggerAdapter)
 }
