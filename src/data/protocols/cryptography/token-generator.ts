8386de5e8c30e33f74f3c04b1754d0413322dbeb
Paulo Cândido <paulo.candido@g.globo>
2021-02-28 17:43:39 -0300

    feat: ensure DbAuthentication calls TokenGenerator with correct values

---
+++ src/data/protocols/cryptography/token-generator.ts
@@ -0,0 +1,5 @@
+export interface TokenGenerator {
+
+  generate(data: any): string
+
+}
--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,4 +1,5 @@
 import { HashComparer } from '@data/protocols/cryptography/hash-comparer'
+import { TokenGenerator } from '@data/protocols/cryptography/token-generator'
 import { LoadAccountByEmailRepository } from '@data/protocols/db/load-account-by-email-repository'
 import { Authentication, AuthenticationModel } from '@domain/usecases'
 import { AuthenticationError } from '@errors/authentication-error'
@@ -8,6 +9,7 @@ export class DbAuthentication implements Authentication {
   constructor(
     private loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private hashComparer: HashComparer,
+    private tokenGenerator: TokenGenerator,
   ) { }
 
   async auth(credentials: AuthenticationModel): Promise<string> {
@@ -18,6 +20,10 @@ export class DbAuthentication implements Authentication {
     const isPasswordCorrect = await this.hashComparer.compare(credentials.password, account.password)
     if (!isPasswordCorrect)
       throw new AuthenticationError()
+
+    const { password, ...accountWithoutPassword } = account
+
+    const token = this.tokenGenerator.generate(accountWithoutPassword)
 
     return ''
   }
