f2900ff1246012b43d5fd3ebfd408f7eb1f25172
Paulo Cândido <paulo.candido@g.globo>
2021-03-11 22:11:50 -0300

    refactor: create a helper method to sut creation on JwtAdapter test

--- src/infra/cryptography/jwt-adapter/jwt-adapter.unit.test.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.unit.test.ts
@@ -11,23 +11,25 @@ jest.mock('jsonwebtoken', () => ({
   },
 }))
 
+const makeSut = (): JwtAdapter => new JwtAdapter(givenSecretPhrase)
+
 describe('JwtAdapter', () => {
 
   it('should call sign with correct values', () => {
-    const sut = new JwtAdapter(givenSecretPhrase)
+    const sut = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
     sut.generate(givenPayload)
     expect(signSpy).toBeCalledWith(givenPayload, givenSecretPhrase)
   })
 
   it('should return the jwt result', () => {
-    const sut = new JwtAdapter(givenSecretPhrase)
+    const sut = makeSut()
     const token = sut.generate(givenPayload)
     expect(token).toBe(generatedToken)
   })
 
   it('should not handle Jwt errors', () => {
-    const sut = new JwtAdapter(givenSecretPhrase)
+    const sut = makeSut()
     const givenError = new Error('any_error')
     jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw givenError })
     expect(() => sut.generate(givenPayload)).toThrow(givenError)
