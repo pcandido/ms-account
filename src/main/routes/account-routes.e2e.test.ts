d874057a4bc6d76d79cb4997929053180f0da848
Paulo Cândido <paulo.candido@g.globo>
2021-03-12 20:56:34 -0300

    feat: configure login route

--- src/main/routes/account-routes.e2e.test.ts
+++ src/main/routes/account-routes.e2e.test.ts
@@ -1,11 +1,14 @@
 import request from 'supertest'
 import app from '@main/config/app'
 import { MongoHelper } from '@infra/db/mongodb/helpers/mogodb-helper'
+import { Collection } from 'mongodb'
 
 describe('SignUp Routes', () => {
 
+  let accountsCollection: Collection
+
   beforeEach(async () => {
-    const accountsCollection = await MongoHelper.getCollection('collections')
+    accountsCollection = await MongoHelper.getCollection('accounts')
     await accountsCollection.deleteMany({})
   })
 
@@ -37,6 +40,60 @@ describe('SignUp Routes', () => {
         .post(givenRoute)
         .send(givenAccount)
         .expect(201)
+    })
+  })
+
+  describe('POST /login', () => {
+    const givenRoute = '/api/login'
+
+    const givenEmail = 'valid@mail.com'
+    const givenPassword = 'password'
+    const givenHashedPassword = '$2b$12$BQwxpEG4DiXrJIj7EvTiFOzbRlkctrKB9pgajOmhyqY1uXYfV4mAu'
+
+    const givenAccount = {
+      name: 'any name',
+      email: givenEmail,
+      password: givenHashedPassword,
+    }
+
+    beforeEach(async () => {
+      await accountsCollection.insertOne(givenAccount)
+    })
+
+    it('should return 401 if provided email does not exists', async () => {
+      const givenCredentials = {
+        email: 'unexistent@mail.com',
+        password: givenPassword,
+      }
+
+      await request(app)
+        .post(givenRoute)
+        .send(givenCredentials)
+        .expect(401)
+    })
+
+    it('should return 401 if provided password is incorrect', async () => {
+      const givenCredentials = {
+        email: givenEmail,
+        password: 'invalid_password',
+      }
+
+      await request(app)
+        .post(givenRoute)
+        .send(givenCredentials)
+        .expect(401)
+    })
+
+    it('should return 200 if credentials are valid', async () => {
+      const givenCredentials = {
+        email: givenEmail,
+        password: givenPassword,
+      }
+
+      await request(app)
+        .post(givenRoute)
+        .send(givenCredentials)
+        .expect(200)
     })
   })
 
--- src/main/routes/account-routes.ts
+++ src/main/routes/account-routes.ts
@@ -1,11 +1,13 @@
-import { makeSignUpController } from '@main/factories/signup'
-import { adaptRoute } from '@main/adapters/express-request-adapter'
 import { Router } from 'express'
+import { adaptRoute } from '@main/adapters/express-request-adapter'
+import { makeSignUpController } from '@main/factories/signup'
+import { makeLoginController } from '@main/factories/login'
 
 export default (): Router => {
   const router = Router()
 
   router.route('/signup').post(adaptRoute(makeSignUpController()))
+  router.route('/login').post(adaptRoute(makeLoginController()))
 
   return router
 }
