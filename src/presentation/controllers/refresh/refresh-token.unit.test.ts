8be0e23d5e06821b32d93bb268162cee103e1c3a
Paulo Cândido <paulo.candido@g.globo>
2021-03-13 18:38:01 -0300

    test: ensure RefreshTokenController calls validator with correct value

--- src/presentation/controllers/refresh/refresh-token.unit.test.ts
+++ src/presentation/controllers/refresh/refresh-token.unit.test.ts
@@ -1,22 +1,35 @@
 import { RefreshTokenController } from './refresh-token'
-import { MissingParamError } from '@presentation/errors'
-import { badRequest } from '@presentation/helpers/http-helper'
+import { Validator } from '@presentation/protocols'
 
 interface SutTypes {
   sut: RefreshTokenController
+  validatorStub: Validator
+}
+
+const makeValidatorStub = () => {
+  class ValidatorStub implements Validator {
+    validate(): void {
+      /* do nothing */
+    }
+  }
+
+  return new ValidatorStub()
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new RefreshTokenController()
-  return { sut }
+  const validatorStub = makeValidatorStub()
+  const sut = new RefreshTokenController(validatorStub)
+  return { sut, validatorStub }
 }
 
 describe('Refresh Token Controller', () => {
 
-  it('should return 400 if no refresh-token is provided', async () => {
-    const { sut } = makeSut()
-    const result = await sut.handle({})
-    expect(result).toEqual(badRequest(new MissingParamError('refreshToken')))
+  it('should call validator with correct value', async () => {
+    const { sut, validatorStub } = makeSut()
+    const validateSpy = jest.spyOn(validatorStub, 'validate')
+    const givenToken = { refreshToken: 'refresh_token' }
+    await sut.handle({ body: givenToken })
+    expect(validateSpy).toBeCalledWith(givenToken)
   })
 
 })
