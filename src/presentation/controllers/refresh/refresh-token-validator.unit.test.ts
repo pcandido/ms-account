27f4c0368abe69cad3e18ea63d4b124301e6fd74
Paulo Cândido <paulo.candido@g.globo>
2021-03-13 18:30:35 -0300

    test: ensure refreshTokenValidator does not throw if no validator fails

--- src/presentation/controllers/refresh/refresh-token-validator.unit.test.ts
+++ src/presentation/controllers/refresh/refresh-token-validator.unit.test.ts
@@ -6,9 +6,15 @@ const makeSut = () => refreshTokenValidator()
 describe('RefreshTokenValidator', () => {
 
   it('should throw if no refreshToken is provided', async () => {
-    const sut  = makeSut()
+    const sut = makeSut()
     const givenRequest = {}
     expect(() => sut.validate(givenRequest)).toThrow(new MissingParamError('refreshToken'))
+  })
+
+  it('should not throw if no validator throws', () => {
+    const sut = makeSut()
+    const givenRequest = { refreshToken: 'any_refresh_token' }
+    expect(() => sut.validate(givenRequest)).not.toThrow()
   })
 
 })
