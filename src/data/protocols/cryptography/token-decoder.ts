3a0cc76e19426f3a666cfa25e45fd65620925686
Paulo Cândido <paulo.candido@g.globo>
2021-03-13 21:33:46 -0300

    feat(refresh-token): ensoure it calls TokenDecoder with correct values

---
+++ src/data/protocols/cryptography/token-decoder.ts
@@ -0,0 +1,5 @@
+export interface TokenDecoder {
+
+  decode(token: string): any
+
+}
--- src/data/usecases/refresh-token/db-refresh-token.ts
+++ src/data/usecases/refresh-token/db-refresh-token.ts
@@ -1,3 +1,4 @@
+import { TokenDecoder } from '@data/protocols/cryptography/token-decoder'
 import { TokenVerifier } from '@data/protocols/cryptography/token-verifier'
 import { TokenSet } from '@domain/models'
 import { RefreshToken } from '@domain/usecases'
@@ -6,11 +7,14 @@ export class DbRefreshToken implements RefreshToken {
 
   constructor(
     private tokenVerifier: TokenVerifier,
+    private tokenDecoder: TokenDecoder,
   ) { }
 
   async refresh(refreshToken: string): Promise<TokenSet | null> {
     const isValid = this.tokenVerifier.verify(refreshToken)
     if (!isValid) return null
+
+    const payload = this.tokenDecoder.decode(refreshToken)
 
 
 
