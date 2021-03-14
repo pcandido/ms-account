a785b27dd5689ca77324bc09263e4df637869cef
Paulo Cândido <paulo.candido@g.globo>
2021-03-13 21:42:26 -0300

    feat(refresh-token): ensoure it calls LoadAccountByEmail with correct value

--- src/data/usecases/refresh-token/db-refresh-token.ts
+++ src/data/usecases/refresh-token/db-refresh-token.ts
@@ -1,5 +1,6 @@
 import { TokenDecoder } from '@data/protocols/cryptography/token-decoder'
 import { TokenVerifier } from '@data/protocols/cryptography/token-verifier'
+import { LoadAccountByEmailRepository } from '@data/protocols/db/account/load-account-by-email-repository'
 import { TokenSet } from '@domain/models'
 import { RefreshToken } from '@domain/usecases'
 
@@ -8,13 +9,15 @@ export class DbRefreshToken implements RefreshToken {
   constructor(
     private tokenVerifier: TokenVerifier,
     private tokenDecoder: TokenDecoder,
+    private loadAccountByEmailRepository: LoadAccountByEmailRepository,
   ) { }
 
   async refresh(refreshToken: string): Promise<TokenSet | null> {
     const isValid = this.tokenVerifier.verify(refreshToken)
     if (!isValid) return null
 
-    const payload = this.tokenDecoder.decode(refreshToken)
+    const { email } = this.tokenDecoder.decode(refreshToken)
+    const account = await this.loadAccountByEmailRepository.loadByEmail(email)
 
 
 
