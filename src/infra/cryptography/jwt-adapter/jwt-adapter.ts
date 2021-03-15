d34a33cdc390802a84d2d08722326730705a87a2
Paulo Cândido <paulo.candido@g.globo>
2021-03-14 22:14:25 -0300

    feat(jwt-adapter): ensure it calls jwt with correct values

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -1,8 +1,9 @@
 import jwt from 'jsonwebtoken'
 import { TokenGenerator } from '@data/protocols/cryptography/token-generator'
 import { TokenSet } from '@domain/models'
+import { TokenVerifier } from '@data/protocols/cryptography/token-verifier'
 
-export class JwtAdapter implements TokenGenerator {
+export class JwtAdapter implements TokenGenerator, TokenVerifier {
 
   constructor(
     private secretPhrase: string,
@@ -12,6 +13,12 @@ export class JwtAdapter implements TokenGenerator {
     const accessToken = jwt.sign({ ...data, tokenType: 'access' }, this.secretPhrase, { expiresIn: '10 minutes' })
     const refreshToken = jwt.sign({ ...data, tokenType: 'refresh' }, this.secretPhrase, { expiresIn: '10 days' })
     return { accessToken, refreshToken }
+  }
+
+  verify(token: string): boolean {
+    jwt.verify(token, this.secretPhrase)
+
+    return true
   }
 
 }
