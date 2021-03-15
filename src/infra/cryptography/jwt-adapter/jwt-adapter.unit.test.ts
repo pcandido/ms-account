3d8785709c570f0d8e466aba51d8a9a1b8ef6182
Paulo Cândido <paulo.candido@g.globo>
2021-03-14 22:14:34 -0300

    test(jwt-adapter): ensure it calls jwt with correct values

--- src/infra/cryptography/jwt-adapter/jwt-adapter.unit.test.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.unit.test.ts
@@ -4,10 +4,14 @@ import { JwtAdapter } from './jwt-adapter'
 const givenSecretPhrase = 'secret_phrase'
 const givenPayload = { field1: 1, field2: '2' }
 const generatedToken = 'generated_token'
+const givenRefreshToken = 'refresh_token'
 
 jest.mock('jsonwebtoken', () => ({
   sign(): string {
     return generatedToken
+  },
+  verify(): boolean {
+    return true
   },
 }))
 
@@ -15,35 +19,46 @@ const makeSut = (): JwtAdapter => new JwtAdapter(givenSecretPhrase)
 
 describe('JwtAdapter', () => {
 
-  it('should call sign with correct values', () => {
-    const sut = makeSut()
-    const signSpy = jest.spyOn(jwt, 'sign')
-    sut.generate(givenPayload)
-    expect(signSpy).toHaveBeenNthCalledWith(1, { ...givenPayload, tokenType: 'access' }, givenSecretPhrase, { expiresIn: expect.anything() })
-    expect(signSpy).toHaveBeenNthCalledWith(2, { ...givenPayload, tokenType: 'refresh' }, givenSecretPhrase, { expiresIn: expect.anything() })
-  })
+  describe('generate method', () => {
+    it('should call sign with correct values', () => {
+      const sut = makeSut()
+      const signSpy = jest.spyOn(jwt, 'sign')
+      sut.generate(givenPayload)
+      expect(signSpy).toHaveBeenNthCalledWith(1, { ...givenPayload, tokenType: 'access' }, givenSecretPhrase, { expiresIn: expect.anything() })
+      expect(signSpy).toHaveBeenNthCalledWith(2, { ...givenPayload, tokenType: 'refresh' }, givenSecretPhrase, { expiresIn: expect.anything() })
+    })
 
-  it('should return the jwt result', () => {
-    const sut = makeSut()
-    const generatedAccessToken = 'accessToken'
-    const generatedRefreshToken = 'refreshToken'
-    jest.spyOn(jwt, 'sign')
-      .mockImplementationOnce(() => generatedAccessToken)
-      .mockImplementationOnce(() => generatedRefreshToken)
+    it('should return the jwt result', () => {
+      const sut = makeSut()
+      const generatedAccessToken = 'accessToken'
+      const generatedRefreshToken = 'refreshToken'
+      jest.spyOn(jwt, 'sign')
+        .mockImplementationOnce(() => generatedAccessToken)
+        .mockImplementationOnce(() => generatedRefreshToken)
 
-    const token = sut.generate(givenPayload)
+      const token = sut.generate(givenPayload)
 
-    expect(token).toEqual({
-      accessToken: generatedAccessToken,
-      refreshToken: generatedRefreshToken,
+      expect(token).toEqual({
+        accessToken: generatedAccessToken,
+        refreshToken: generatedRefreshToken,
+      })
+    })
+
+    it('should not handle Jwt errors', () => {
+      const sut = makeSut()
+      const givenError = new Error('any_error')
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw givenError })
+      expect(() => sut.generate(givenPayload)).toThrow(givenError)
     })
   })
 
-  it('should not handle Jwt errors', () => {
-    const sut = makeSut()
-    const givenError = new Error('any_error')
-    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw givenError })
-    expect(() => sut.generate(givenPayload)).toThrow(givenError)
+  describe('verify method', () => {
+    it('should call jwt.verify with correct value', () => {
+      const sut = makeSut()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      sut.verify(givenRefreshToken)
+      expect(verifySpy).toBeCalledWith(givenRefreshToken, givenSecretPhrase)
+    })
   })
 
 })
