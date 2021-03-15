5a887d57d40de6f229740f62175c986c6cb827a1
Paulo Cândido <paulo.candido@g.globo>
2021-03-14 22:46:21 -0300

    refactor: remove TokenVerifier as TokenDecoder will verify token

--- src/data/usecases/refresh-token/db-refresh-token.ts
+++ src/data/usecases/refresh-token/db-refresh-token.ts
@@ -1,6 +1,5 @@
 import { TokenDecoder } from '@data/protocols/cryptography/token-decoder'
 import { TokenGenerator } from '@data/protocols/cryptography/token-generator'
-import { TokenVerifier } from '@data/protocols/cryptography/token-verifier'
 import { LoadAccountByEmailRepository } from '@data/protocols/db/account/load-account-by-email-repository'
 import { TokenSet } from '@domain/models'
 import { RefreshToken } from '@domain/usecases'
@@ -8,17 +7,16 @@ import { RefreshToken } from '@domain/usecases'
 export class DbRefreshToken implements RefreshToken {
 
   constructor(
-    private tokenVerifier: TokenVerifier,
     private tokenDecoder: TokenDecoder,
     private loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private tokenGenerator: TokenGenerator,
   ) { }
 
   async refresh(refreshToken: string): Promise<TokenSet | null> {
-    const isValid = this.tokenVerifier.verify(refreshToken)
-    if (!isValid) return null
+    const payload = this.tokenDecoder.decode(refreshToken)
+    if (!payload) return null
 
-    const { email, tokenType } = this.tokenDecoder.decode(refreshToken)
+    const { email, tokenType } = payload
     if (tokenType !== 'refresh') return null
 
     const account = await this.loadAccountByEmailRepository.loadByEmail(email)
--- src/data/usecases/refresh-token/db-refresh-token.unit.test.ts
+++ src/data/usecases/refresh-token/db-refresh-token.unit.test.ts
@@ -1,13 +1,11 @@
 import { TokenDecoder } from '@data/protocols/cryptography/token-decoder'
 import { TokenGenerator } from '@data/protocols/cryptography/token-generator'
-import { TokenVerifier } from '@data/protocols/cryptography/token-verifier'
 import { LoadAccountByEmailRepository } from '@data/protocols/db/account/load-account-by-email-repository'
 import { AccountModel, TokenSet } from '@domain/models'
 import { DbRefreshToken } from './db-refresh-token'
 
 interface SutTypes {
   sut: DbRefreshToken
-  tokenVerifierStub: TokenVerifier
   tokenDecoderStub: TokenDecoder
   loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
   tokenGeneratorStub: TokenGenerator
@@ -29,15 +27,6 @@ const makeTokenSet = () => ({
   accessToken: 'accessToken',
   refreshToken: 'refreshToken',
 })
-
-const makeTokenVerifierStub = (): TokenVerifier => {
-  class TokenVerifierStub implements TokenVerifier {
-    verify(): boolean {
-      return true
-    }
-  }
-  return new TokenVerifierStub()
-}
 
 const makeTokenDecoderStub = (): TokenDecoder => {
   class TokenDecoderStub implements TokenDecoder {
@@ -70,36 +59,14 @@ const makeTokenGeneratorStub = (): TokenGenerator => {
 }
 
 const makeSut = (): SutTypes => {
-  const tokenVerifierStub = makeTokenVerifierStub()
   const tokenDecoderStub = makeTokenDecoderStub()
   const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailRepositoryStub()
   const tokenGeneratorStub = makeTokenGeneratorStub()
-  const sut = new DbRefreshToken(tokenVerifierStub, tokenDecoderStub, loadAccountByEmailRepositoryStub, tokenGeneratorStub)
-  return { sut, tokenVerifierStub, tokenDecoderStub, loadAccountByEmailRepositoryStub, tokenGeneratorStub }
+  const sut = new DbRefreshToken(tokenDecoderStub, loadAccountByEmailRepositoryStub, tokenGeneratorStub)
+  return { sut, tokenDecoderStub, loadAccountByEmailRepositoryStub, tokenGeneratorStub }
 }
 
 describe('DbAuthentication UseCase', () => {
-
-  it('should call TokenVerifier with correct value', async () => {
-    const { sut, tokenVerifierStub } = makeSut()
-    const verifySpy = jest.spyOn(tokenVerifierStub, 'verify')
-    await sut.refresh(givenRefreshToken)
-    expect(verifySpy).toBeCalledWith(givenRefreshToken)
-  })
-
-  it('should return null if TokenVerifier returns false', async () => {
-    const { sut, tokenVerifierStub } = makeSut()
-    jest.spyOn(tokenVerifierStub, 'verify').mockReturnValue(false)
-    const result = await sut.refresh(givenRefreshToken)
-    expect(result).toBeNull()
-  })
-
-  it('should not handle TokenVerifier internal errors', async () => {
-    const { sut, tokenVerifierStub } = makeSut()
-    const givenError = new Error('any error')
-    jest.spyOn(tokenVerifierStub, 'verify').mockImplementationOnce(() => { throw givenError })
-    await expect(() => sut.refresh(givenRefreshToken)).rejects.toThrow(givenError)
-  })
 
   it('should call TokenDecoder with correct value', async () => {
     const { sut, tokenDecoderStub } = makeSut()
@@ -113,6 +80,13 @@ describe('DbAuthentication UseCase', () => {
     const givenError = new Error('any error')
     jest.spyOn(tokenDecoderStub, 'decode').mockImplementationOnce(() => { throw givenError })
     await expect(() => sut.refresh(givenRefreshToken)).rejects.toThrow(givenError)
+  })
+
+  it('should return null if TokenDecoder returns null', async () => {
+    const { sut, tokenDecoderStub } = makeSut()
+    jest.spyOn(tokenDecoderStub, 'decode').mockReturnValueOnce(null)
+    const result = await sut.refresh(givenRefreshToken)
+    expect(result).toBeNull()
   })
 
   it('should return null if the token is not of type refresh', async () => {
