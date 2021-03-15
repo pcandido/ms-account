6f8b8fa756bd5c0bb3c4e7c8360718d3b729c209
Paulo Cândido <paulo.candido@g.globo>
2021-03-14 22:55:55 -0300

    feat: add a refresh-token factory

---
+++ src/main/factories/refresh-token.ts
@@ -0,0 +1,20 @@
+import config from 'config'
+import { Controller } from '@presentation/protocols'
+import { ControllerLogger } from '@presentation/decorators/controller-logger'
+import { JwtAdapter } from '@infra/cryptography/jwt-adapter/jwt-adapter'
+import { AccountMongoRepository } from '@infra/db/mongodb/acount/account-mongo-repository'
+import { ConsoleLoggerAdapter } from '@utils/console-logger-adapter'
+import { DbRefreshToken } from '@data/usecases/refresh-token/db-refresh-token'
+import { refreshTokenValidator } from '@presentation/controllers/refresh/refresh-token-validator'
+import { RefreshTokenController } from '@presentation/controllers/refresh/refresh-token'
+
+export const makeLoginController = (): Controller => {
+  const loadByEmailRepository = new AccountMongoRepository()
+  const jwtSecretPhrase = config.get<string>('app.jwt.secret')
+  const tokenGeneratorAndDecoder = new JwtAdapter(jwtSecretPhrase)
+  const refreshToken = new DbRefreshToken(tokenGeneratorAndDecoder, loadByEmailRepository, tokenGeneratorAndDecoder)
+  const validator = refreshTokenValidator()
+  const refreshTokenController = new RefreshTokenController(validator, refreshToken)
+  const consoleLoggerAdapter = new ConsoleLoggerAdapter()
+  return new ControllerLogger(refreshTokenController, consoleLoggerAdapter)
+}
