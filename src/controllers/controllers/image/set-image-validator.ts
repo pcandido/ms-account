28af79001da679231b245dc8440c0daaa223da38
Paulo Cândido <paulo.candido@g.globo>
2021-04-03 16:22:44 -0300

    feat: ensure setImageValidator include all validators

--- src/controllers/controllers/image/set-image-validator.ts
+++ src/controllers/controllers/image/set-image-validator.ts
@@ -1,4 +1,8 @@
-import { MissingParamValidator } from '@controllers/helpers/validation'
+import { MissingParamValidator, ValidatorComposite } from '@controllers/helpers/validation'
+import { ImageFieldValidator } from '@controllers/helpers/validation/image-field-validator'
 import { Validator } from '@controllers/protocols'
 
-export const setImageValidator = (): Validator => new MissingParamValidator('image')
+export const setImageValidator = (): Validator => new ValidatorComposite([
+  new MissingParamValidator('image'),
+  new ImageFieldValidator('image', 10),
+])
--- src/controllers/controllers/image/set-image-validator.unit.test.ts
+++ src/controllers/controllers/image/set-image-validator.unit.test.ts
@@ -1,5 +1,5 @@
-import { MissingParamError } from '@controllers/errors'
-import { Validator } from '@controllers/protocols'
+import { MissingParamError, InvalidParamError } from '@controllers/errors'
+import { Validator, MultPartFile } from '@controllers/protocols'
 import { setImageValidator } from './set-image-validator'
 
 interface SutTypes {
@@ -11,9 +11,13 @@ const makeSut = (): SutTypes => {
   return { sut }
 }
 
-const makeImage = (): Buffer => {
-  return Buffer.from('any_image', 'utf-8')
-}
+const makeImage = (): MultPartFile => ({
+  originalName: 'any_name',
+  size: 500,
+  encoding: 'utf-8',
+  mimeType: 'image/jpg',
+  buffer: Buffer.from('any_image', 'utf-8'),
+})
 
 describe('SetImageValidator', () => {
 
@@ -21,6 +25,13 @@ describe('SetImageValidator', () => {
     const { sut } = makeSut()
     const givenRequest = {}
     expect(() => sut.validate(givenRequest)).toThrow(new MissingParamError('image'))
+  })
+
+  it('should throw if mime type is not image', async () => {
+    const { sut } = makeSut()
+    const givenRequest = { image: makeImage() }
+    givenRequest.image.mimeType = 'application/json'
+    expect(() => sut.validate(givenRequest)).toThrow(new InvalidParamError('image'))
   })
 
   it('should not throw if no validator throws', () => {
