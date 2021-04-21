ea469eee0b8500a0a1e713c22855b891b7d6b6e6
Paulo Cândido <paulo.candido@g.globo>
2021-04-21 17:14:00 -0300

    feat: ensure PasswordRecoveryUseCase does not handle any LoadAccoutByEmailRepo internal errors

--- src/usecases/usecases/password-recovery/password-recovery-usecase.ts
+++ src/usecases/usecases/password-recovery/password-recovery-usecase.ts
@@ -1,11 +1,11 @@
 import { PasswordRecovery } from '@domain/usecases'
 import { LoadAccountByEmailRepository } from '@usecases/protocols/account/load-account-by-email-repository'
 
-export class PasswordRecoveryUseCase implements PasswordRecovery{
+export class PasswordRecoveryUseCase implements PasswordRecovery {
 
   constructor(
     private loadAccountByEmailRepositry: LoadAccountByEmailRepository,
-  ){}
+  ) { }
 
   async recover(email: string): Promise<void> {
     await this.loadAccountByEmailRepositry.loadByEmail(email)
