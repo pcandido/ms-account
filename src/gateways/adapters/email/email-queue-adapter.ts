f970c477c52da496a0ee975b90d223670620c923
Paulo Cândido <paulo.candido@g.globo>
2021-04-21 20:55:43 -0300

    feat: ensure EmailQueueAdapter calls createChannel

--- src/gateways/adapters/email/email-queue-adapter.ts
+++ src/gateways/adapters/email/email-queue-adapter.ts
@@ -1,14 +1,15 @@
 import { EmailMessage, EmailSender } from '@usecases/protocols/email/email-sender'
-import {connect} from 'amqplib'
+import { connect } from 'amqplib'
 
 export class EmailQueueAdapter implements EmailSender {
 
   constructor(
-    private rabbitmqHost:string,
-  ){}
+    private rabbitmqHost: string,
+  ) { }
 
   async send(message: EmailMessage): Promise<void> {
-    await connect(this.rabbitmqHost)
+    const connection = await connect(this.rabbitmqHost)
+    await connection.createChannel()
   }
 
 }
