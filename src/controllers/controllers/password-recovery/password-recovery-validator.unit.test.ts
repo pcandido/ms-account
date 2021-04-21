445a7c881f9fc017632a2fc7ebe1c8266ed599f9
Paulo Cândido <paulo.candido@g.globo>
2021-04-21 16:29:05 -0300

    refactor: create helper method to create sut on passwordRecoveryValidator tests

--- src/controllers/controllers/password-recovery/password-recovery-validator.unit.test.ts
+++ src/controllers/controllers/password-recovery/password-recovery-validator.unit.test.ts
@@ -1,10 +1,21 @@
 import { passwordRecoveryValidator } from './password-recovery-validator'
 import { MissingParamError } from '@controllers/errors'
+import { Validator } from '@controllrs/protocols'
+
+interface SutTypes {
+  sut: Validator
+}
+
+const makeSut = (): SutTypes => {
+
+  const sut = passwordRecoveryValidator()
+  return { sut }
+}
 
 describe('passwordRecoveryValidator', () => {
 
   it('should throw if no email is provided', () => {
-    const sut = passwordRecoveryValidator()
+    const { sut } = makeSut()
     const givenRequest = {}
     expect(() => sut.validate(givenRequest)).toThrow(new MissingParamError('email'))
   })
