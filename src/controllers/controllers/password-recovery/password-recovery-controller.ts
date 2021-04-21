9490154510b3130dbc9ea617e033423d6d8fd1a6
Paulo Cândido <paulo.candido@g.globo>
2021-04-21 12:24:04 -0300

    feat: ensure PasswordRecoveryController returns server error if validator throws internal error

--- src/controllers/controllers/password-recovery/password-recovery-controller.ts
+++ src/controllers/controllers/password-recovery/password-recovery-controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Request, Response, Validator } from '@controllers/protocols'
-import { badRequest } from '@controllers/helpers/http-helper'
+import { badRequest, serverError } from '@controllers/helpers/http-helper'
+import { ValidationError } from '@controllers/errors'
 
 export class PasswordRecoveryController implements Controller {
 
@@ -10,10 +11,13 @@ export class PasswordRecoveryController implements Controller {
   async handle(request: Request): Promise<Response> {
     try {
       this.validator.validate(request.body)
+      return { statusCode: 200, body: {} }
     } catch (error) {
-      return badRequest(error)
+      if(error instanceof ValidationError)
+        return badRequest(error)
+      else
+        return serverError(error)
     }
-    return { statusCode: 200, body: {} }
   }
 
 }
