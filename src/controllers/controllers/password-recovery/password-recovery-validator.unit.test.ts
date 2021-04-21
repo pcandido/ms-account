7018bf860f71fa006d679e77392b25dd00f6d027
Paulo Cândido <paulo.candido@g.globo>
2021-04-21 16:41:01 -0300

    test: ensure passwordRecoveryValidator throws if an invalid email is provided

--- src/controllers/controllers/password-recovery/password-recovery-validator.unit.test.ts
+++ src/controllers/controllers/password-recovery/password-recovery-validator.unit.test.ts
@@ -1,15 +1,26 @@
 import { passwordRecoveryValidator } from './password-recovery-validator'
-import { MissingParamError } from '@controllers/errors'
-import { Validator } from '@controllrs/protocols'
+import { MissingParamError, InvalidParamError } from '@controllers/errors'
+import { Validator, EmailValidator } from '@controllrs/protocols'
 
 interface SutTypes {
   sut: Validator
+  emailValidatorStub: EmailValidator
+}
+
+const makeEmailValidatorStub = () => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true
+    }
+  }
+
+  return new EmailValidatorStub()
 }
 
 const makeSut = (): SutTypes => {
-
-  const sut = passwordRecoveryValidator()
-  return { sut }
+  const emailValidatorStub = makeEmailValidatorStub()
+  const sut = passwordRecoveryValidator(emailValidatorStub)
+  return { sut, emailValidatorStub }
 }
 
 describe('passwordRecoveryValidator', () => {
@@ -18,6 +29,13 @@ describe('passwordRecoveryValidator', () => {
     const { sut } = makeSut()
     const givenRequest = {}
     expect(() => sut.validate(givenRequest)).toThrow(new MissingParamError('email'))
+  })
+
+  it('should throw if an invalid email is provided', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const givenRequest = { email: 'invalid_email' }
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    expect(() => sut.validate(givenRequest)).toThrow(new InvalidParamError('email'))
   })
 
 })
